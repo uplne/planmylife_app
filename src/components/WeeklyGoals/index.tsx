3348d6eb6a4f1146a3e4a985125ca22b56d4494b
Steve <uplneinak@gmail.com>
2024-02-04 08:15:40 +0000

    Fix subheading for weekly goals

--- src/components/WeeklyGoals/index.tsx
+++ src/components/WeeklyGoals/index.tsx
@@ -1,12 +1,12 @@
-import { Box } from '../Box';
-import { SubHeading } from '../SubHeading';
-import { TasksBox } from '../TasksBox';
+import { Box } from "../Box";
+import { SubHeading } from "../SubHeading";
+import { TasksBox } from "../TasksBox";
 
 export const WeeklyGoals = () => {
   return (
     <TasksBox>
       <Box>
-        <SubHeading title="Goals and Projects" />
+        <SubHeading>Goals and Projects</SubHeading>
       </Box>
     </TasksBox>
   );

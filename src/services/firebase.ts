8a1bbe239a33ef74a919d8c693b17a15bd0e3a8c
Steve <uplneinak@gmail.com>
2024-05-03 10:12:15 +0100

    Remove API key

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -7,21 +7,21 @@ import {
   getRedirectResult,
 } from "firebase/auth";
 import { getFirestore, Timestamp } from "firebase/firestore";
-import { getFunctions } from 'firebase/functions';
+import { getFunctions } from "firebase/functions";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDIt5QgV5lsEQBh0TTCtdMhudiyPudXKBU",
+  // put back API key
   authDomain: "planmylife-1c7fd.firebaseapp.com",
   databaseURL: "https://planmylife-1c7fd.firebaseio.com",
   projectId: "planmylife-1c7fd",
   storageBucket: "planmylife-1c7fd.appspot.com",
   messagingSenderId: "29592412564",
   appId: "1:29592412564:web:d0e289b682fb42c1cd2f81",
-  measurementId: "G-VTDC7Q4VEN"
+  measurementId: "G-VTDC7Q4VEN",
 };
 
 // Initialize Firebase
@@ -38,11 +38,6 @@ export const functions = getFunctions(app);
 
 export const GoogleAuth = new GoogleAuthProvider();
 
-export type CollectionType = 'default' | 'recurring';
+export type CollectionType = "default" | "recurring";
 
-export {
-  GoogleAuthProvider,
-  signInWithRedirect,
-  getRedirectResult,
-  Timestamp,
-};
+export { GoogleAuthProvider, signInWithRedirect, getRedirectResult, Timestamp };
